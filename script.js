26b2c29d9b81bb924ab851fe556d2ffa80fb4091
ellis <elrestea@gmail.com>
2026-05-12 15:31:21 +0000

    begin step four; book info integration still needs fixing

--- script.js
+++ script.js
@@ -28,23 +28,50 @@ console.log(myLibrary)
 
 // step 3
 
-const listContainer = document.getElementById('book-list')
+function renderLibrary(){
 
-myLibrary.forEach(book => {
 
-const bookDiv = document.createElement('div');
-bookDiv.className = "book-card"
 
-bookDiv.innerHTML = `
-    <h1>${book.title}</h1>
-    <h3>Author: ${book.author}</h3>
-    <h3>Pages: ${book.pages}</h3>
-    <h3>Read before?: ${book.read}</h3>
-    <p>${book.id}</p>
-`;
 
-listContainer.appendChild(bookDiv);
+    const listContainer = document.getElementById('book-list')
 
-});
+    myLibrary.forEach(book => {
+
+    const bookDiv = document.createElement('div');
+    bookDiv.className = "book-card"
+
+    bookDiv.innerHTML = `
+        <h1>${book.title}</h1>
+        <h3>Author: ${book.author}</h3>
+        <h3>Pages: ${book.pages}</h3>
+        <h3>Read before?: ${book.read}</h3>
+        <p>${book.id}</p>
+    `;
+
+    listContainer.appendChild(bookDiv);
+
+    });
+
+}
 
 // step 4
+const btn = document.getElementById("new-book-btn");
+
+btn.addEventListener("click", () => {
+    console.log("clicked");
+    document.getElementById("book-form").style.display = "block";
+})
+
+const form = document.getElementById("book-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  })
+
+const title = document.getElementById("title").value;
+const author = document.getElementById("author").value;
+const pages = document.getElementById("pages").value;
+const read = document.getElementById("read").checked;
+
+addBookToLibrary(title, author, pages, read);
+renderLibrary();
